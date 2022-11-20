538290d7cab1199e16e35ac7f73c77f66054bf05
Tandrima Singha <78292851+tandrimasingha@users.noreply.github.com>
2022-11-20 19:43:05 +0530

    Update LEFT FUNCTION & RIGHT FUNC.sql

--- LEFT FUNCTION & RIGHT FUNC.sql
+++ LEFT FUNCTION & RIGHT FUNC.sql
@@ -15,3 +15,11 @@ FROM Customers;
 # Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
 
 Select DISTINCT CITY from STATION where RIGHT(CITY,1) IN ('a','e','i','o','u');
+
+# Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) IN ('A','E','I','O','U') AND RIGHT(CITY,1) IN ('a','e','i','o','u');
+
+# Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U') OR RIGHT(CITY,1)  NOT IN ('a','e','i','o','u');
